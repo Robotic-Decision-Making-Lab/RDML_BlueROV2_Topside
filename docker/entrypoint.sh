d9d64727a7f20bdc26dae1c983e4237db9f19efc
Evan Palmer <evanp922@gmail.com>
2026-09-16 22:46:06 -0700

    Address PR comments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Source ROS 2
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-# Build the mounted workspace
 cd /home/ubuntu/ws_ros
 colcon build
 
-# Source the workspace, if built
 if [ -f /home/ubuntu/ws_ros/install/setup.bash ]
 then
   source /home/ubuntu/ws_ros/install/setup.bash
 fi
 
-# Execute the command passed into this entrypoint
 exec "$@"
